af34461b67a90d73d06905449b3694587846bb9f
Jnwoke <jnwoke@appzonegroup.com>
2024-08-13 08:37:27 +0100

    :recycle:

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useState, useEffect } from "react";
 import WalletModal from "./Modal";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const MainContent = () => {
  const wall=   { endpoint: "tet", id: 2, name: "weiner" };
@@ -54,12 +57,13 @@ const MainContent = () => {
    setKeystore(false);
  };
   return (
-    <div className="main-content">
-    <div className='about'>
+    <Container className="main-content">
+      <Row className='check'>
+    <Col xs={12} md={8}  className='about'>
       <h1 className='weiner'>WienerAI</h1>
       <h2>Part Dog, Part Sausage, Part AI Trading Bot</h2>
-    </div>
-    <div className='about2'>
+    </Col>
+    <Col xs={12} md={4} className='about2'>
         <div className='inner'>
       <p>$WAI Claim Now Live</p>
       <p>You Can Now Claim Your $WAI Tokens. Plus, Stake Your Tokens To Earn Rewards!</p>
@@ -75,8 +79,9 @@ const MainContent = () => {
       <WalletModal show={show} handleClose={handleClose} name={name} />
       </div>
       </div>
-      </div>
-    </div>
+      </Col>
+      </Row>
+    </Container>
   );
 };
 
